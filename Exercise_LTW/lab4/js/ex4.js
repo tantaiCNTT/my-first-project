184de8151ca67e94e82e66ef2da7ed0a99af2ea0
tantaiCNTT <54395383+tantaiCNTT@users.noreply.github.com>
2019-10-04 18:52:16 +0700

    Create ex4.js

---
+++ Exercise_LTW/lab4/js/ex4.js
@@ -0,0 +1,17 @@
+
+function  check(s1, s2) {
+    var count, l1 ,l2;
+    count = 0;
+    var arr1 = s1.split(" ");
+    var arr2 = s2.split(" ");
+    l1 = arr1.length;
+    l2 = arr2.length;
+    for( let i = 0; i < l2; i++) {
+        for( let j = 0 ; j< l1; j++) {
+            if(arr1[i] == arr2[j]) {
+
+            }
+        }
+    }
+
+}
