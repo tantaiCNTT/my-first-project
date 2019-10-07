531287702d4b5169e61a450afa036e0ac16225e6
tantaiCNTT <54395383+tantaiCNTT@users.noreply.github.com>
2019-10-07 20:48:21 +0700

    Update ex2.js

--- LTWeb/lab4/js/ex2.js
+++ LTWeb/lab4/js/ex2.js
@@ -1,39 +1,39 @@
-function getContent() {
-    var tmp1 = document.getElementById('input_a');
-    var tmp2 = document.getElementById("input_b");
-    var tmp3 = document.getElementById("input_c");
-    var tmp4 = document.getElementById("view_equation");
-    var tmp5 =document.getElementById("result");
-    var a = tmp1.value;
-    var b = tmp2.value;
-    var c = tmp3.value;
-    if(a ==1) {
-        a="";
+function showView() {
+    var input_a = document.getElementById('a');
+    var input_b= document.getElementById('b');
+    var input_c = document.getElementById('c');
+    var view_equation = document.getElementById('view');
+    var view_result = document.getElementById('result');
+    var a, b, c;
+    var formatEquation;
+    a = input_a.value;
+    b = input_b.value;
+    c = input_c.value;
+    if( a == 1) {
+        a = "";
     }
-    tmp4.value = a+"x^2" +" + "+ b+"x" +" + "+ c +" = " + 0;
-    tmp5.value = result(a,b,c);
+    formatEquation = a+"x"+"^2"+" + "+b+"x "+"+ "+c+" = "+ 0;
+    view_equation.value = formatEquation;
+    view_result.value = result(a, b, c);
 
 }
-function result(a, b, c) {
-    var x1, x2, delta, result;
-     if( a == 0) {
-         x1 = (-c)/b;
-         result ="x"+ "="+x1;
-     } else {
-         delta = b*b - 4*a*c;
-          if(delta < 0) {
-              result ="PT vô nghiệm!"
-          } else if(delta > 0) {
-              x1 = (-b -Math.sqrt(delta)/ 2*a);
-              x2 = (-b +Math.sqrt(delta)/ 2*a);
-              result ="x1= "+x1+","+"x2" + "= " +x2;
-          } else if(delta == 0) {
-              x1 = x2 = -b / 2*a;
-              result="x1 = x2 = " + x1;
-          }
-     }
-     return result;
+function result(a ,b ,c ) {
+    var delta, x1, x2, tmp ="";
+    if( a == 0) {
+        x1 = (-c) / b;
+        tmp = "x= "+x1;
+    } else {
+           delta = b*b - 4*a*c;
+           if(delta > 0) {
+               x1 = (-b - Math.sqrt(delta))/ 2*a;
+               x2 = (-b + Math.sqrt(delta))/ 2*a;
+               tmp ="x1 = "+ x1 +"," +"x2 = "+ x2;
+           } else if(delta < 0) {
+               tmp ="Phương trình vô nghiệm!"
+           } else if( delta == 0) {
+               x1 = x2 = (-b)/ 2*a;
+               tmp = "x1 = x2 ="+ x1;
+           }
+    }
+    return tmp;
 }
-
-
-
